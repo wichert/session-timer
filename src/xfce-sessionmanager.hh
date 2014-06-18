1507a43e5de18109335f9e96e84c0c4c3e5a99b1
Wichert Akkerman <wichert@wiggy.net>
2014-06-18 15:15:34 +0200

    Use a complete Xfce session manager proxy.

--- src/xfce-sessionmanager.hh
+++ src/xfce-sessionmanager.hh
@@ -4,7 +4,7 @@
 #include "xfce-sessionmanager-proxy.hh"
 
 
-class XfceSessionManagerProxy : public org::xfce::SessionManager_proxy,
+class XfceSessionManagerProxy : public org::xfce::Session::Manager_proxy,
 	public DBus::IntrospectableProxy,
 	public DBus::ObjectProxy {
 public:
@@ -12,6 +12,19 @@ public:
 				const char *path="/org/xfce/SessionManager",
 				const char* name="org.xfce.SessionManager") :
 		DBus::ObjectProxy(connection, path, name) {
+	}
+
+	virtual void ShutdownCancelled() {
+	}
+
+	virtual void ClientRegistered(const std::string&) {
+	}
+
+	virtual void StateChanged(const uint32_t&, const uint32_t&) {
+	}
+
+	void Logout(bool show_dialog, bool allow_save) {
+		org::xfce::Session::Manager_proxy::Logout(show_dialog, allow_save);
 	}
 };
 
