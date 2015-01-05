bd3d10aed9d15b4db7e858f1d9da913265cf4ca8
Wichert Akkerman <wichert@wiggy.net>
2015-01-05 13:48:50 +0100

    Add basic epoll abstraction

---
+++ src/manager/poller.cc
@@ -0,0 +1,49 @@
+#include <cerrno>
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <system_error>
+#include "poller.hh"
+
+using namespace std;
+
+Poller::Poller() : fds(), epoll_fd(-1) {
+	if ((epoll_fd=epoll_create1(EPOLL_CLOEXEC))==-1)
+		throw system_error(errno, system_category());
+}
+
+
+Poller::~Poller() {
+	close(epoll_fd);
+}
+
+
+void Poller::add_to_epoll(shared_ptr<Pollee> pollee) {
+	epoll_event ev;
+	ev.events=EPOLLIN;
+	ev.data.ptr=pollee.get();
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pollee->fd(), &ev)==-1)
+		if (errno!=EEXIST)
+			throw system_error(errno, system_category());
+}
+
+void Poller::remove_from_epoll(shared_ptr<Pollee> pollee) {
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, pollee->fd(), nullptr)==-1)
+		if (errno!=ENOENT)
+			throw system_error(errno, system_category());
+}
+
+
+void Poller::run() {
+	epoll_event events[10];
+	int event_count, i;
+
+	while (!empty()) {
+		if ((event_count=epoll_wait(epoll_fd, events, 10, -1))==-1)
+			throw system_error(errno, system_category());
+		for (i=0; i<event_count; i++) {
+			auto p = reinterpret_cast<Pollee*>(events[i].data.ptr);
+			p->update();
+		}
+	}
+}
+
---
+++ src/manager/poller.hh
@@ -0,0 +1,118 @@
+#ifndef MGR_POLLER_INCLUDED
+#define MGR_POLLER_INCLUDED
+
+#include <memory>
+#include <set>
+
+class Pollee {
+public:
+	virtual ~Pollee() {}
+	virtual int fd() const = 0;
+	virtual void update() = 0;
+};
+
+
+class Poller {
+public:
+	typedef std::set<std::shared_ptr<Pollee>> _cont_type;
+
+	typedef _cont_type::key_type key_type;
+	typedef _cont_type::value_type value_type;
+	typedef _cont_type::size_type size_type;
+	typedef _cont_type::iterator iterator;
+	typedef _cont_type::const_iterator const_iterator;
+
+private:
+	_cont_type fds;
+	int epoll_fd;
+
+	void add_to_epoll(std::shared_ptr<Pollee> pollee);
+	void remove_from_epoll(std::shared_ptr<Pollee> pollee);
+
+public:
+	Poller();
+	~Poller();
+
+	Poller(const Poller&) = delete;
+	Poller& operator=(const Poller&) = delete;
+
+	void run();
+
+	iterator begin() noexcept {
+		return fds.begin();
+	}
+
+	const_iterator begin() const noexcept {
+		return fds.begin();
+	}
+
+	const_iterator cbegin() const noexcept {
+		return fds.begin();
+	}
+
+	iterator end() noexcept {
+		return fds.end();
+	}
+
+	const_iterator end() const noexcept {
+		return fds.end();
+	}
+
+	const_iterator cend() const noexcept {
+		return fds.end();
+	}
+
+	bool empty() const noexcept {
+		return fds.empty();
+	}
+
+	size_type size() const noexcept {
+		return fds.size();
+	}
+
+	size_type max_size() const noexcept {
+		return fds.max_size();
+	}
+
+	void clear() noexcept {
+		fds.clear();
+	}
+
+	std::pair<iterator, bool> insert(const value_type& value) {
+		add_to_epoll(value);
+		return fds.insert(value); // Remove from epoll on error?
+	}
+
+	std::pair<iterator, bool> insert(value_type&& value) {
+		add_to_epoll(value);
+		return fds.insert(value); // Remove from epoll on error?
+	}
+
+	iterator erase(const_iterator pos) {
+		return fds.erase(pos);
+	}
+
+	iterator erase(const_iterator first, const_iterator last) {
+		return fds.erase(first, last);
+	}
+
+	size_type erase(const key_type& key) {
+		return fds.erase(key);
+	}
+
+	size_type count(const key_type& key) const {
+		return fds.count(key);
+	}
+
+	iterator find(const key_type& key) {
+		return fds.find(key);
+	}
+
+	const_iterator find(const key_type& key) const {
+		return fds.find(key);
+	}
+
+};
+
+#endif
+
